4408d85ec7a9675debadfc958f650d6682330b01
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-03-06 20:42:07 +0100

    Clean up style violations and refactor code

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      "src/js/inverted-index.js",
+      "src/js/invertedIndex.js",
       "jasmine/app-test.js"
     ],
 
